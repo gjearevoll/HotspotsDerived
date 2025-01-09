201b9c67af2aa3228e69aa1820ffe0630ac7a5bf
Sam Wenaas Perrin <samp@wallace.win.ntnu.no>
2025-01-09 09:30:35 +0000

    Cleaned up script

--- modelResizing.R
+++ modelResizing.R
@@ -1,15 +1,24 @@
+
+
+#### MODEL RESIZING ####
+
+# Ron has created a function which drastically reduces the size of the models without losing any of the key ingredients.
+# We apply this to every model that comes out of the pipeline.
+
 library(lobstr)
-# We need to transfer every vascular plants model from BioDivMapping over to here
+# We need to tidentify the correct directory for finding our models first.
 hotspotsDir <- "../BioDivMapping/data/run_2025-01-06/modelOutputs"
 
+# Import Ron's function
 source("functions/reset_environments.R")
 
-# Get every file in there with vascular plants involved
+# Get every richness model listed
 dirsWithModel <- list.files(hotspotsDir, recursive = TRUE, pattern = "*richnessModel.rds", full.names = TRUE)
 dirsWithModelShort <- list.files(hotspotsDir, recursive = TRUE, pattern = "*richnessModel.rds")
 taxaWithModels <- gsub(paste0(hotspotsDir, "/"), "", unique(gsub('[[:digit:]]+', '', sub("\\/.*", "", dirsWithModelShort))))
-#taxaWithModels <- "birds"
+#taxaWithModels <- "spider"
 
+# Now loop through each group and reduce the model size
 for (taxa in taxaWithModels) {
   dirsWithModelTaxa <- dirsWithModel[grep(paste0("/",taxa), dirsWithModel)]
   for (i in seq_along(dirsWithModelTaxa)) {
