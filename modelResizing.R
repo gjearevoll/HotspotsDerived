7a7aa3f0aaf16f08e7f28123a0da2bb5e35f14cb
Sam Wenaas Perrin <samp@wallace.win.ntnu.no>
2025-04-08 08:46:27 +0000

    Enabled mdoelResizing for terminal

--- modelResizing.R
+++ modelResizing.R
@@ -1,4 +1,14 @@
 
+###----------------------###
+### 0. Bash preparation ####
+###----------------------###
+args <- commandArgs(trailingOnly = TRUE)
+
+start <- Sys.time()
+
+dateUsed <- as.character(args[1])
+taxaWithModels <- as.character(args[2])
+setwd("~/HotspotsDerived")
 
 #### MODEL RESIZING ####
 
@@ -7,26 +17,29 @@
 
 library(lobstr)
 # We need to tidentify the correct directory for finding our models first.
-hotspotsDir <- "../BioDivMapping/data/run_2025-01-06/modelOutputs"
+hotspotsDir <- paste0("../BioDivMapping/data/run_",dateUsed,"/modelOutputs")
 
 # Import Ron's function
-source("functions/reset_environments.R")
+source("../HotspotsDerived/functions/reset_environments.R")
 
 # Get every richness model listed
 dirsWithModel <- list.files(hotspotsDir, recursive = TRUE, pattern = "*richnessModel.rds", full.names = TRUE)
 dirsWithModelShort <- list.files(hotspotsDir, recursive = TRUE, pattern = "*richnessModel.rds")
-taxaWithModels <- gsub(paste0(hotspotsDir, "/"), "", unique(gsub('[[:digit:]]+', '', sub("\\/.*", "", dirsWithModelShort))))
-#taxaWithModels <- "spider"
+#taxaWithModels <- gsub(paste0(hotspotsDir, "/"), "", unique(gsub('[[:digit:]]+', '', sub("\\/.*", "", dirsWithModelShort))))
+#taxaWithModels <- "vascularPlantsB"
 
 # Now loop through each group and reduce the model size
 for (taxa in taxaWithModels) {
   dirsWithModelTaxa <- dirsWithModel[grep(paste0("/",taxa), dirsWithModel)]
   for (i in seq_along(dirsWithModelTaxa)) {
     modelName <- dirsWithModelTaxa[[i]]
-    richnessModel <- readRDS(modelName)
-    #obj_size(richnessModel)
-    reducedModel <- reset_environments(richnessModel)
-    saveRDS(reducedModel, modelName)
-    if (i %% 10 == 0) cat(i, " models complete for ", taxa, "\n")
+    if (file.info(modelName)[1,"size"] > 300000000) {
+      richnessModel <- readRDS(modelName)
+      #obj_size(richnessModel)
+      reducedModel <- reset_environments(richnessModel)
+      saveRDS(reducedModel, modelName)
+    }
+    
+    if (i %% 3 == 0) cat(i, " models complete for ", taxa, "\n")
   }
 }
