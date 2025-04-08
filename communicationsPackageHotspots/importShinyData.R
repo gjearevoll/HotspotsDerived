7f70c8a72eb89d4089ea7ebeb0ad4851acaa7755
Sam Wenaas Perrin <samp@wallace.win.ntnu.no>
2025-04-08 08:45:54 +0000

    Send all data to importedData folder

--- communicationsPackageHotspots/importShinyData.R
+++ communicationsPackageHotspots/importShinyData.R
@@ -1,19 +1,38 @@
 ### Need to get all necessary data into a folder, and then aggregate it all
+
+### --------------------------------------------- ###
+#### 0. Set base parameters and import libraries ####
+### --------------------------------------------- ###
+
 library(dplyr)
 library(terra)
 
-relevantTaxa <- c("vascularPlants", "hymenopterans", "birds")
+relevantTaxa <- c("vascularPlants", "insects", "birds", "fungi", "lichens")
 appDirectory <- "communicationsPackageHotspots/data"
-baseDirectory <- "../BioDivMapping/data/run_2024-10-11/modelOutputs/processedOutputs/"
+importedDirectory <- paste0(appDirectory, "/importedData")
+baseDirectory <- "../BioDivMapping/data/run_2025-01-06/modelOutputs/processedOutputs/"
+
+if (!dir.exists(importedDirectory)) {
+  create.dir(importedDirectory)
+}
+
+### ----------------------------------------- ###
+#### 1. Import red list and Norwegian border ####
+### ----------------------------------------- ###
 
 # Import red list
 source("functions/importRedList.R")
 redList <- importRedList(c("VU", "EN", "CR"))
 redList$simpleScientificName <- gsub(" ", "_", redList$species)
-saveRDS(redList, "communicationsPackageHotspots/data/redList.RDS")
+saveRDS(redList, "communicationsPackageHotspots/data/importedData/redList.RDS")
 
-# Import all richness data
-richnessDirectory <- "data/modelOutputs"
+norwayBorder2 <- sf::read_sf("data/norge_border/Noreg_polygon.shp")
+
+### ------------------------- ###
+#### 2. Import richness list ####
+### ------------------------- ###
+
+richnessDirectory <- "data/modelOutputs/run_2025-01-06"
 
 availableFiles <- list.files(richnessDirectory, full.names = TRUE)
 filesToImport <- c(sapply(relevantTaxa, FUN = function(taxa) {
@@ -24,54 +43,55 @@ for (taxaFile in filesToImport) {
   print(paste("aggregating ", taxaFile))
   starterRast <- rast(taxaFile)
   aggRast <- aggregate(starterRast, fact = 10)
-  fileName <- paste0(appDirectory, "/", gsub("data/modelOutputs/", "", taxaFile))
+  fileName <- paste0(importedDirectory, "/", gsub(paste0(richnessDirectory,"/"), "", taxaFile))
   writeRaster(aggRast, fileName, overwrite = TRUE)
 }
 
-# Import all beta diversity data
-betaDirectory <- "betaDiversity/data"
+### -------------------------- ###
+#### 3. Import beta diversity ####
+### -------------------------- ###
+
+betaDirectory <- "processes/betaDiversity/data"
 availableFiles <- list.files(betaDirectory, full.names = TRUE)
+availableFiles <- grep("10", availableFiles, value = TRUE)
 filesToImport <- c(sapply(relevantTaxa, FUN = function(taxa) {
   allFiles <- availableFiles[grep(taxa, availableFiles)]
-  allFiles[grep("10", allFiles)]
+  allFiles
 }))
 
 for (taxaFile in filesToImport) {
   print(paste("aggregating ", taxaFile))
   starterRast <- rast(taxaFile)
-  aggRast <- aggregate(starterRast, fact = 10)
-  fileName <- paste0(appDirectory, "/", gsub("betaDiversity/data/", "", taxaFile))
+  starterRastCropped <- crop(starterRast, project(vect(norwayBorder2), starterRast), mask = T)
+  aggRast <- aggregate(starterRastCropped, fact = 10)
+  fileName <- paste0(importedDirectory, "/", gsub("processes/betaDiversity/data/", "", taxaFile))
   writeRaster(aggRast, fileName, overwrite = TRUE)
 }
 
-# Import all hotspots data
-hotspotDirectory <- "overlays/data/hotspots"
-availableFiles <- list.files(hotspotDirectory, full.names = TRUE)
-filesToImport <- c(sapply(relevantTaxa, FUN = function(taxa) {
-  taxaGrouping <- ifelse(taxa == "birds", "fugler", ifelse(taxa == "vascularPlants", "karplanter", "insekter"))
-  fileNames <- availableFiles[grep(taxaGrouping, availableFiles)]
-  fileNames[!grepl("xml", fileNames)]
-}))
+### -------------------- ###
+#### 4. Import Hotspots ####
+### -------------------- ###
 
-for (taxaFile in filesToImport) {
-  print(paste("aggregating ", taxaFile))
-  starterRast <- rast(taxaFile)
-  aggRast <- aggregate(starterRast, fact = 10, fun = "modal", na.rm=TRUE)
-  fileName <- paste0(appDirectory, "/", gsub("overlays/data/hotspots/", "", taxaFile))
-  writeRaster(aggRast, fileName, overwrite = TRUE)
-}
+# This requires you to have run processes/createSpeciesHotspots.R
+hotspotFile <- rast("data/allHotspots.tiff")
+aggRast <- aggregate(hotspotFile, fact = 10, fun = "modal", na.rm=TRUE)
+fileName <- paste0(importedDirectory, "/allHotspots.tiff")
+writeRaster(aggRast, fileName, overwrite = TRUE)
 
-# Define ansvararter to plot
-ansvarsarter <- readRDS("data/ansvarsArterList.RDS")
+### ----------------------------------- ###
+#### 5. Import individual species data ####
+### ----------------------------------- ###
+
+# Define species to plot
+speciesToPlot <- read.csv("communicationsPackageHotspots/data/speciesForDisplay.csv", sep = ",")
 namesFromGroups <- do.call(rbind, lapply(relevantTaxa, FUN = function(taxa) {
   taxaNames <- names(rast(paste0(baseDirectory, "speciesprobability_", 
                          taxa, ".tiff")))
   df <- data.frame(species = taxaNames, taxa = taxa)
-  df <- df[df$species %in% ansvarsarter$simpleScientificName,]
-  df$fullName <- ansvarsarter$GBIFName[match(df$species, ansvarsarter$simpleScientificName)]
+  df <- df[df$species %in% speciesToPlot$species,]
+  df$fullName <- gsub("_"," ", df$species)
   df
-})) %>% group_by(taxa) %>%
-  sample_n(4)
+}))
 focalSpeciesDDVector <- split(namesFromGroups$fullName, f = namesFromGroups$taxa)
 focalSpeciesDDList <- lapply(focalSpeciesDDVector, FUN = function(x) {
   speciesList <- as.list(x)
@@ -79,8 +99,8 @@ focalSpeciesDDList <- lapply(focalSpeciesDDVector, FUN = function(x) {
   speciesList
 }
 )
-saveRDS(focalSpeciesDDList, "communicationsPackageHotspots/data/ddList.RDS")
-saveRDS(namesFromGroups, "communicationsPackageHotspots/data/namesFromGroups.RDS")
+saveRDS(focalSpeciesDDList, "communicationsPackageHotspots/data/importedData/ddList.RDS")
+saveRDS(namesFromGroups, "communicationsPackageHotspots/data/importedData/namesFromGroups.RDS")
 
 
 # Import all individual data
@@ -90,28 +110,18 @@ for (taxa in relevantTaxa) {
   fullUncertaintyList <- rast(paste0(baseDirectory, "speciesuncertainty_", 
                           taxa, ".tiff"))
   narrowedList <- fullList[[namesFromGroups$species[namesFromGroups$taxa %in% taxa]]]
-  aggNarrowedList <- aggregate(narrowedList, fact = 10)
+  narrowedListCropped <- crop(narrowedList, project(vect(norwayBorder2), narrowedList), mask = T)
+  aggNarrowedList <- aggregate(narrowedListCropped, fact = 10)
   narrowedUList <- fullUncertaintyList[[namesFromGroups$species[namesFromGroups$taxa %in% taxa]]]
-  aggnarrowedUList <- aggregate(narrowedUList, fact = 10)
-  writeRaster(aggNarrowedList, paste0(appDirectory, "/", taxa, "_probability.tiff"), overwrite = TRUE)
-  writeRaster(aggnarrowedUList, paste0(appDirectory, "/", taxa, "_uncertainty.tiff"), overwrite = TRUE)
+  narrowedUListCropped <- crop(narrowedUList, project(vect(norwayBorder2), narrowedUList), mask = T)
+  aggnarrowedUList <- aggregate(narrowedUListCropped, fact = 10)
+  writeRaster(aggNarrowedList, paste0(importedDirectory, "/", taxa, "_probability.tiff"), overwrite = TRUE)
+  writeRaster(aggnarrowedUList, paste0(importedDirectory, "/", taxa, "_uncertainty.tiff"), overwrite = TRUE)
 }
 
-# Import images
-# We need to download photos from iNaturalist, including their URL and the user name of the individual
-# who took the photo to give appropriate credit
+# Import images from iNaturalist
 
-# # Check if there is already an image credit file
-# if (file.exists("visualisation/hotspotMaps/data/imageCredit.RDS")) {
-#   imageCredit <- readRDS("visualisation/hotspotMaps/data/imageCredit.RDS")
-# } else {
-#   imageCredit <- data.frame(species = redList$species[redList$valid],
-#                             credit = NA,
-#                             url = NA,
-#                             stringsAsFactors = FALSE)
-# }
-
-taxaFolder <- "communicationsPackageHotspots/data/photos/"
+taxaFolder <- "communicationsPackageHotspots/data/importedData/photos/"
 if (!dir.exists(taxaFolder)) { dir.create(taxaFolder)}
 imageCredit <- data.frame(species = unlist(focalSpeciesDDList),
                           credit = NA,
