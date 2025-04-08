d205684cabdd720bae29dfea9a50b3f6d0b40fd5
Sam Wenaas Perrin <samp@wallace.win.ntnu.no>
2025-04-08 09:19:50 +0000

    Update with extent fix

--- processes/betaDiversity/createBetaDiversityPackage.R
+++ processes/betaDiversity/createBetaDiversityPackage.R
@@ -10,25 +10,26 @@ focalTaxa <- read.csv("data/focalTaxa.csv")
 
 betaDirectory <- "processes/betaDiversity/data/"
 
+baseRaster <- rast("processes/betaDiversity/data/fungi10betaDiversity.tiff")
+
 ###---------------------###
 ### 1. Combine rasters ####
 ###---------------------###
-rastersToImport <- c("vascularPlants", "birds")
-rasterPackage <- rast(paste0(betaDirectory, rastersToImport, "10betaDiversity.tiff")) |>
-  setNames(rastersToImport)
-
-### Fungi import
-fungiLayer <- rast(paste0(betaDirectory, "fungi10betaDiversity.tiff")) |>
-  terra::project(rasterPackage, method = "mode") |>
-  setNames("fungi")
+rastersToImport <- grep("10", list.files(betaDirectory), value = TRUE)
+rasterPackage <- lapply(rastersToImport, FUN = function(x) {
+  rawR <- rast(paste0(betaDirectory, x))
+  if (ext(rawR) != ext(baseRaster)) {
+    return(project(rawR, baseRaster, "mode"))
+  } else {return(rawR)}
+}) |> setNames(gsub("10betaDiversity.tiff", "", rastersToImport))
+rasterPackage <- rast(rasterPackage)
 
 # Get border to map to
 norwayBorder <- st_union(nor_municip_map_b2024_default_sf)
 norwayBorderProjected <- fillHoles(terra::project(vect(norwayBorder), rasterPackage[[1]]))
 
 # Crop to better border
-rasterPackageCropped <- c(rasterPackage, fungiLayer) |>
-  crop(norwayBorderProjected)
+rasterPackageCropped <- crop(rasterPackage, norwayBorderProjected)
 
 ### Save to file 
 writeRaster(rasterPackageCropped, filename = paste0(betaDirectory, "rasterPackage4.tiff"), overwrite = TRUE)
